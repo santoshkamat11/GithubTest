5bce6bf8b7f8214395d4b176bf9835514d1dddd1
SantoshKamat <SantoshKamat@DESKTOP-2VC7HT5>
2020-09-05 09:14:25 +0530

    remove comments

--- GithubTest/src/com/pranav/MainClient.java
+++ GithubTest/src/com/pranav/MainClient.java
@@ -5,8 +5,8 @@ public class MainClient {
 	public static void main(String[] args) {
 		
 		Pig myPig = new Pig();  // Create a Pig object
-	    myPig.animalSound(); // print animal sound
-	    myPig.sleep(); // print animal sleep
+	    myPig.animalSound(); 
+	    myPig.sleep(); 
 
 	}
 
