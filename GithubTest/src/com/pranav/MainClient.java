cd145aa31fdff7ebc9c8b352ac65da44c300606e
SantoshKamat <SantoshKamat@DESKTOP-2VC7HT5>
2020-09-05 09:55:34 +0530

    simple commit

--- GithubTest/src/com/pranav/MainClient.java
+++ GithubTest/src/com/pranav/MainClient.java
@@ -4,10 +4,17 @@ public class MainClient {
 
 	public static void main(String[] args) {
 		
-		Pig myPig = new Pig();  // Create a Pig object
-	    
+		int y = subtract(9,4);
 		
+		System.out.println(y);
 		
+	}
+
+	private static int subtract(int i, int j) {
+		// add method
+		int k = i - j;
+		
+		return k;
 	}
 
 }
