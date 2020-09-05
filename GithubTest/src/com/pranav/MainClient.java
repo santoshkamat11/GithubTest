c635a57bcc2566b40429de9327b7f325ef572961
Santosh kamat <pranavkamat11@gmail.com>
2020-09-05 09:50:12 +0530

    Update MainClient.java
    add method through remote

--- GithubTest/src/com/pranav/MainClient.java
+++ GithubTest/src/com/pranav/MainClient.java
@@ -5,9 +5,20 @@ public class MainClient {
 	public static void main(String[] args) {
 		
 		Pig myPig = new Pig();  // Create a Pig object
+		
+		int x = add(9,4);
+		
+		System.out.println(x);
 	    
 		
 		
+	}
+	
+	private static int add(int i, int j) {
+		// add method
+		int k = i + j;
+		
+		return k;
 	}
 
 }
