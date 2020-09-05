dac8e4d2cd25b6793dc4cd908f0535028e900e33
SantoshKamat <SantoshKamat@DESKTOP-2VC7HT5>
2020-09-05 10:12:55 +0530

    subtarct function

--- GithubTest/src/com/pranav/MainClient.java
+++ GithubTest/src/com/pranav/MainClient.java
@@ -6,25 +6,17 @@ public class MainClient {
 		
         Pig myPig = new Pig();  // Create a Pig object
 		
-		int x = add(9,4);
-		
-		System.out.println(x);
-		
 		int y = subtract(9,4);
 		
 		System.out.println(y);
+		
+		
 		
 	}
 
 	
 		
-	private static int add(int i, int j) {
-		// add method
-		int k = i + j;
-		
-		return k;
-	}
-
+	
 	private static int subtract(int i, int j) {
 		// add method
 		int k = i - j;
