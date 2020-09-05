a86b46f98d7cced95b9abb3f879c812dbae40c8d
SantoshKamat <SantoshKamat@DESKTOP-2VC7HT5>
2020-09-05 09:07:11 +0530

    second commit

---
+++ GithubTest/src/com/pranav/MainClient.java
@@ -0,0 +1,13 @@
+package com.pranav;
+
+public class MainClient {
+
+	public static void main(String[] args) {
+		
+		Pig myPig = new Pig();  // Create a Pig object
+	    myPig.animalSound();
+	    myPig.sleep();
+
+	}
+
+}
