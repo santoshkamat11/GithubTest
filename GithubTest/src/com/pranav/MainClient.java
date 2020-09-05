694125368e2d99c321fe60e466a6727c2fc4d427
Santosh kamat <pranavkamat11@gmail.com>
2020-09-05 09:09:36 +0530

    Update MainClient.java
    commit through remote to test pull

--- GithubTest/src/com/pranav/MainClient.java
+++ GithubTest/src/com/pranav/MainClient.java
@@ -5,8 +5,8 @@ public class MainClient {
 	public static void main(String[] args) {
 		
 		Pig myPig = new Pig();  // Create a Pig object
-	    myPig.animalSound();
-	    myPig.sleep();
+	    myPig.animalSound(); // print animal sound
+	    myPig.sleep(); // print animal sleep
 
 	}
 
