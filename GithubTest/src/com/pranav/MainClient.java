48a17bea1fcf0a9410f9285eeb6e68ee00eb0bef
SantoshKamat <SantoshKamat@DESKTOP-2VC7HT5>
2020-09-05 10:29:02 +0530

    subtract function

--- GithubTest/src/com/pranav/MainClient.java
+++ GithubTest/src/com/pranav/MainClient.java
@@ -5,24 +5,13 @@ public class MainClient {
 	public static void main(String[] args) {
 		
         Pig myPig = new Pig();  // Create a Pig object
-		
-        int x = add(9,4);
-        System.out.println(x);
+	
         
         int y = subtract(9,4);
 		System.out.println(y);
 		
 		
 		
-	}
-
-	
-		
-	private static int add(int i, int j) {
-		// add method
-		int k = i + j;
-		
-		return k;
 	}
 
 	private static int subtract(int i, int j) {
