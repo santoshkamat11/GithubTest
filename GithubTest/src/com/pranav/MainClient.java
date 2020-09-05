fa6d31643bd1bb4b32c1f106ad46bfbc9931960f
Santosh kamat <pranavkamat11@gmail.com>
2020-09-05 09:16:13 +0530

    Update MainClient.java
    remove animal sound statement

--- GithubTest/src/com/pranav/MainClient.java
+++ GithubTest/src/com/pranav/MainClient.java
@@ -5,7 +5,7 @@ public class MainClient {
 	public static void main(String[] args) {
 		
 		Pig myPig = new Pig();  // Create a Pig object
-	    myPig.animalSound(); 
+	    
 	    myPig.sleep(); 
 
 	}
